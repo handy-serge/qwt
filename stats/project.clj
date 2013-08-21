47d7277bcb75acfac225d62d7fa830970d3007d8
ssmolau <ssmolau@handyem.com>
2013-08-21 16:10:26 -0400

    SS-402 Add hiccups to dependencies to be able to generate HTML in ClojureScript.

--- stats/project.clj
+++ stats/project.clj
@@ -2,24 +2,25 @@
   :description
   "# Statictics display in ClojureScript to be used by Cytodyem. #
 
-The project displays statistic information on a right side of the Cytodyem window.
-See the [Powerpoint on a Wiki page for more information][1].
+  The project displays statistic information on a right side of the Cytodyem window.
+  See the [Powerpoint on a Wiki page for more information][1].
 
-The statistic display should include:
+  The statistic display should include:
 
-- Tabular representation of statistical data:
-    - Columns contain separate statistics values
-    - Lines represent plots and gates
-- Statistics is updated from Cytodyem's C++/CLI code.
+  - Tabular representation of statistical data:
+      - Columns contain separate statistics values
+      - Lines represent plots and gates
+  - Statistics is updated from Cytodyem's C++/CLI code.
 
-[1]: https://handyem.atlassian.net/wiki/display/SS/Alvin+release+scope
+  [1]: https://handyem.atlassian.net/wiki/display/SS/Alvin+release+scope
 "
 
   :url "https://handyem.atlassian.net/wiki/display/SS/Statistics+view"
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [domina "1.0.2-SNAPSHOT"]]
+                 [domina "1.0.2-SNAPSHOT"]
+                 [hiccups "0.2.0"]]
   :plugins [[lein-ring "0.8.6"]
             [lein-cljsbuild "0.3.2"]]
 
