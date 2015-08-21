9f6ef18ac07685db61c3df45af10e29d05384e78
ssmolau <ssmolau@handyem.com>
2015-08-21 13:03:48 -0400

    SS-1572 Added plot to the main form layout.

--- qt_experiment/QwtScatter/MainWindow.cpp
+++ qt_experiment/QwtScatter/MainWindow.cpp
@@ -1,9 +1,14 @@
 #include "MainWindow.h"
+#include "ScatterPlot.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
+    , m_plot(new ScatterPlot(this))
 {
     ui.setupUi(this);
+
+    // Add plot to the main window at the top of the plot layout.
+    ui.plotAreaLayout->insertWidget(0, m_plot);
 
     // Setup connect to ui signals.
     connect(ui.exitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
--- qt_experiment/QwtScatter/ScatterPlot.cpp
+++ qt_experiment/QwtScatter/ScatterPlot.cpp
@@ -3,4 +3,12 @@
 ScatterPlot::ScatterPlot( QWidget* parent)
     : QWidget(parent)
 {
+    // Changing background color and size of the widget to see if
+    // it is correctly added to layout.
+    setAutoFillBackground(true);
+    auto colors = palette();
+    colors.setColor(backgroundRole(), Qt::darkGray);
+    setPalette(colors);
+    setMinimumSize(200, 600);
+    setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
 }
